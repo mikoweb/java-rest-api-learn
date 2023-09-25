74fe5aae772b5101b522bec0b72ea964c952ab81
Rafał Mikołajun <root@rmweb.pl>
2023-09-25 18:57:10 +0200

    data serializer

--- src/main/java/app/module/user/application/controller/UserController.java
+++ src/main/java/app/module/user/application/controller/UserController.java
@@ -1,19 +1,23 @@
 package app.module.user.application.controller;
 
 import app.module.user.infrastructure.dao.UserDao;
-import app.module.user.infrastructure.entity.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import java.util.List;
 
 @Path("users")
 public class UserController {
     @Path("all")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getUsers() {
-        return UserDao.getUsers();
+    public String getUsers() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        return mapper.writeValueAsString(UserDao.getUsers());
     }
 }
