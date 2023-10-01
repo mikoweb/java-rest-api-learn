0ab3e54a2be366a1565e7d10319bf2d166d7cc57
Rafał Mikołajun <root@rmweb.pl>
2023-10-01 21:23:49 +0200

    user DAO

--- src/main/java/app/module/user/infrastructure/dao/UserDao.java
+++ src/main/java/app/module/user/infrastructure/dao/UserDao.java
@@ -4,12 +4,30 @@ import app.module.user.infrastructure.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class UserDao {
-    public static List<User> getUsers() {
-        List<User> users = new ArrayList<>();
-        users.add(new User(1, "test", "test", "test"));
+    private static final List<User> users = new ArrayList<>();
 
+    public static List<User> getUsers() {
         return users;
+    }
+
+    public static void addUser(User user) {
+        users.add(user);
+    }
+
+    public static void updateUserEmail(int id, String email) {
+        Optional<User> userValue = findUserById(id);
+        userValue.ifPresent(user -> user.setEmail(email));
+    }
+
+    public static void deleteUser(int id) {
+        Optional<User> userValue = findUserById(id);
+        userValue.ifPresent(users::remove);
+    }
+
+    private static Optional<User> findUserById(int id) {
+        return users.stream().filter(user -> user.getId() == id).findFirst();
     }
 }
