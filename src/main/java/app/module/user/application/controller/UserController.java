b0b7e09d8881f08034bad0e23600667de5abed9a
Rafał Mikołajun <root@rmweb.pl>
2023-10-01 21:23:40 +0200

    user endpoints

--- src/main/java/app/module/user/application/controller/UserController.java
+++ src/main/java/app/module/user/application/controller/UserController.java
@@ -1,12 +1,11 @@
 package app.module.user.application.controller;
 
 import app.module.user.infrastructure.dao.UserDao;
+import app.module.user.infrastructure.entity.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("users")
@@ -19,5 +18,40 @@ public class UserController {
         mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 
         return mapper.writeValueAsString(UserDao.getUsers());
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("add")
+    public User addUser(
+        @FormParam("id") int id,
+        @FormParam("name") String name,
+        @FormParam("password") String password,
+        @FormParam("email") String email
+    ) {
+        User user = new User(id, name, password, email);
+        UserDao.addUser(user);
+
+        return user;
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("update/{id}")
+    public String updateUserEmail(@PathParam("id") int id, @FormParam("email") String email) {
+        UserDao.updateUserEmail(id, email);
+
+        return "email updated!";
+    }
+
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("delete/{id}")
+    public String deleteUser(@PathParam("id") int id) {
+        UserDao.deleteUser(id);
+
+        return "user deleted!";
     }
 }
