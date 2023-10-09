7bb8312492e5b350e63b399ad61a59a30b7b14c4
Rafał Mikołajun <root@rmweb.pl>
2023-10-09 16:41:43 +0200

    update User entity

--- src/main/java/app/module/user/infrastructure/entity/User.java
+++ src/main/java/app/module/user/infrastructure/entity/User.java
@@ -1,9 +1,14 @@
 package app.module.user.infrastructure.entity;
 
+import jakarta.persistence.*;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String password;
