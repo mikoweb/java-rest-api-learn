a86f141a85fc411392569e5968b68524840c77fc
Rafał Mikołajun <root@rmweb.pl>
2023-10-10 17:00:18 +0200

    add lombok package

--- src/main/java/app/module/user/infrastructure/entity/User.java
+++ src/main/java/app/module/user/infrastructure/entity/User.java
@@ -2,8 +2,12 @@ package app.module.user.infrastructure.entity;
 
 import jakarta.persistence.*;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @XmlRootElement
+@Data
+@RequiredArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -14,45 +18,10 @@ public class User {
     private String password;
     private String email;
 
-    public User() {
-    }
-
     public User(int id, String name, String password, String email) {
         this.id = id;
         this.name = name;
         this.password = password;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 }
